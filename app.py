9decc786795c3d9eb962c557d568a3678a58313e
jabyliao <jabyliao@gmail.com>
2022-06-03 23:52:56 +0800

    step3 main frame huan

--- app.py
+++ app.py
@@ -18,54 +18,10 @@ def data():
     result = data.to_dict(orient='records')
     seq = [[item['time'], item['market-price'], item['trade-volume']] for item in result]
     return jsonify(seq)
- 
 
 @app.route("/noAI")
 def noAI():
     return render_template('indexNoAI.html')    
-
-@app.route("/setRandom")
-def getData():
-    myserver ="localhost"
-    myuser="test123"
-    mypassword="test123"
-    mydb="aiotdb"
-    
-    debug =0
-    from  pandas import DataFrame as df
-    import pandas as pd                     # 引用套件並縮寫為 pd
-    import numpy as np
-
-    import pymysql.cursors
-    #db = mysql.connector.connect(host="140.120.15.45",user="toto321", passwd="12345678", db="lightdb")
-    #conn = mysql.connector.connect(host=myserver,user=myuser, passwd=mypassword, db=mydb)
-    conn = pymysql.connect(host=myserver,user=myuser, passwd=mypassword, db=mydb)
-
-    c = conn.cursor()
- 
-
-    #====== 執行 MySQL 查詢指令 ======#
-    c.execute("update sensors set value = RAND()*1000 where true")
-    conn.commit()
-    
-    c.execute("SELECT * FROM sensors")
-
-    #====== 取回所有查詢結果 ======#
-    results = c.fetchall()
-    print(type(results))
-    print(results[:10])
-    if debug:
-        input("pause ....select ok..........")
-
-    test_df = df(list(results),columns=['id','time','value','temp','humi','status'])
-
-    print(test_df.head(10))
-    result = test_df.to_dict(orient='records')
-    seq = [[item['id'], item['time'], item['value'], item['temp'], item['humi'], item['status']] for item in result]
-    return jsonify(seq)
-    ######### cursor close, conn close
-    c.close()
-    conn.close()
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
